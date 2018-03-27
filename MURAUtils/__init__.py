68347b2040c26a12b29cbbb204cfb07bc9285dd8
Stefan Iarca <stefan@Stefans-MacBook-Pro.local>
2018-03-27 15:53:42 +0300

    Added new

---
+++ MURAUtils/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+from . import densenet
+from . import heatmap
+from . import modelop
+
+__version__ = '1.0.0'
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='MURAUtils',
+      version='1.0.0',
+      description='Package 1',
+      author='Stefan Iarca',
+      author_email='iasotm@yahoo.com',
+      url='https://github.com/Stexan/MURAUtils',
+      license='MIT',
+      install_requires=['torch'],
+      classifiers=[
+                   ''
+                  ]
+                   ,
+      packages=['MURAUtils'])
